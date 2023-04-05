c861a74f287cf74f73f88d69cdf5c7fbcea74de3
felipehenrique159 <felipecod6@hotmail.com>
2023-04-04 22:41:39 -0300

    Componente de mensages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { MessagesComponent } from './components/messages/messages.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/moment/moment.service.ts
+++ src/app/services/moment/moment.service.ts
@@ -15,6 +15,4 @@ export class MomentService {
     return this.http.post<FormData>(this.apiUrl, formData)
   }
 
-
-
 }
